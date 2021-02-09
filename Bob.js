d0888a836c6e91084e9d8624060e6631aa9cce4b
Shreyman360 <74798357+Shreyman360@users.noreply.github.com>
2021-02-09 16:20:45 -0500

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,30 @@
+class Bob {
+    constructor(x,y,radius){
+
+       var options={
+       
+           isStatic:false,    
+           density:0.8,
+           friction:0,
+           restitution:1
+           
+       }
+           
+       this.body= Bodies.circle(x,y,radius/2,options);
+       this.radius=radius;
+       World.add(world,this.body);
+   }
+
+   display(){
+       var pos=this.body.position;
+       push();
+       translate(pos.x,pos.y);
+       rectMode(CENTER);
+       ellipse(0,0,this.radius,this.radius);
+       pop();
+   }
+
+}  
+
+
+
